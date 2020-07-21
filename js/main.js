63596e9e8a3e05359bb9cbbaa5155795fb09ace4
sethyeung123 <seth.yeung@osmiumcg.com>
2020-07-21 18:49:39 +0800

    Nearly Final Move

--- js/main.js
+++ js/main.js
@@ -11,3 +11,52 @@ $('.carousel-item', '.show-neighbors').each(function(){
   }
   prev.children(':nth-last-child(2)').clone().prependTo($(this));
 });
+
+// Sticky Shares
+(function($) {
+	"use strict"
+
+var $shares = $('.sticky-container .sticky-shares'),
+$sharesHeight = $shares.height(),
+$sharesTop,
+$sharesCon = $('.sticky-container'),
+$sharesConTop,
+$sharesConleft,
+$sharesConHeight,
+$sharesConBottom,
+$offsetTop = 150;
+
+function setStickyPos () {
+  if ($shares.length > 0) {
+    $sharesTop = $shares.offset().top
+    $sharesConTop = $sharesCon.offset().top;
+    $sharesConleft = $sharesCon.offset().left;
+    $sharesConHeight = $sharesCon.height();
+    $sharesConBottom = $sharesConHeight + $sharesConTop;
+  }
+}
+
+function stickyShares (wScroll) {
+  if ($shares.length > 0) {
+    if ( $sharesConBottom - $sharesHeight - $offsetTop < wScroll ) {
+      $shares.css({ position: 'absolute', top: $sharesConHeight - $sharesHeight , left:0});
+    } else if ( $sharesTop < wScroll + $offsetTop ) {
+      $shares.css({ position: 'fixed', top: $offsetTop, left: $sharesConleft });
+    } else {
+      $shares.css({position: 'absolute', top: 0, left: 0});
+    }
+  }
+}
+
+$(window).on('scroll', function() {
+  stickyShares($(this).scrollTop());
+});
+
+$(window).resize(function() {
+  setStickyPos();
+  stickyShares($(this).scrollTop());
+});
+
+setStickyPos();
+
+})(jQuery);
